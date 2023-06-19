e2aded9249f37071bb59708d3a18fb5a6107c51e
Nguyen Phong <nguyenphong.mobile.engineer@gmail.com>
2023-06-19 13:39:10 +0700

    Initial Commit

---
+++ multi-level-tca-swiftui/multi_level_tca_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  multi_level_tca_swiftuiApp.swift
+//  multi-level-tca-swiftui
+//
+//  Created by Nguyen Phong on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct multi_level_tca_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
